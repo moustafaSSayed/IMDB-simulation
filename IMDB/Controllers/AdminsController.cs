3d9c10b1567ded67d630bb5c17899e3e0153e231
yousef mostafa <yousef.m.saber2001@gmail.com>
2022-05-06 23:36:47 +0200

    new database

---
+++ IMDB/Controllers/AdminsController.cs
@@ -0,0 +1,150 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using IMDB.Models;
+
+namespace IMDB.Controllers
+{
+    public class AdminsController : Controller
+    {
+        private IMDb_dataEntities db = new IMDb_dataEntities();
+
+        // GET: Admins
+        public ActionResult Index()
+        {
+            return View(db.Admins.ToList());
+        }
+
+        
+
+       
+
+
+        // GET: Actors/Edit/5
+        public ActionResult EditActor(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Actor actor = db.Actors.Find(id);
+            if (actor == null)
+            {
+                return HttpNotFound();
+            }
+            return View(actor);
+        }
+
+        // POST: Actors/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        
+        public ActionResult EditActor(Actor actor , HttpPostedFileBase file)
+        {
+
+            if (ModelState.IsValid)
+            {
+                if (file != null)
+                {
+                    string pic = System.IO.Path.GetFileName(file.FileName);
+                    string path = System.IO.Path.Combine(Server.MapPath("//path l sora elly ht7otoha "));
+
+                    file.SaveAs(path);
+                    actor.actor_photo = pic;
+                }
+                db.Entry(actor).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(actor);
+        }
+
+        // GET: Movies/Edit/5
+        public ActionResult EditMovie(string id)
+        {
+            if (id == null)
+            {
+
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Movy movie = db.Movies.Find(id);
+            if (movie == null)
+            {
+                return HttpNotFound();
+            }
+            return View(movie);
+        }
+
+        // POST: Movies/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+   
+        public ActionResult EditMovie(Movy movie, HttpPostedFileBase file)
+        {
+            if (ModelState.IsValid)
+            {
+                if (file != null)
+                {
+                    string pic = System.IO.Path.GetFileName(file.FileName);
+                    string path = System.IO.Path.Combine(Server.MapPath("//path l sora elly ht7otoha "));
+
+                    file.SaveAs(path);
+                    movie.movies_photo = pic;
+                }
+                db.Entry(movie).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(movie);
+        }
+
+        // GET: Director/Edit/5
+        public ActionResult EditDirector(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Director director = db.Directors.Find(id);
+            if (director == null)
+            {
+                return HttpNotFound();
+            }
+            return View(director);
+        }
+
+        // POST: Director/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        
+        public ActionResult EditD(Director director, HttpPostedFileBase file)
+        {
+            if (ModelState.IsValid)
+            {
+                if (file != null)
+                {
+                    string pic = System.IO.Path.GetFileName(file.FileName);
+                    string path = System.IO.Path.Combine(Server.MapPath("//path l sora elly ht7otoha "));
+
+                    file.SaveAs(path);
+                    director.director_photo = pic;
+                }
+                db.Entry(director).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(director);
+        }
+
+
+
+    }
+}
